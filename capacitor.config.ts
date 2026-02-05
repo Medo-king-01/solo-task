fdf59c7116e2cd87944cd4a1af0d8b7a17b5ff0a
Medo-king-01 <medokingpromax@gmail.com>
2026-02-06 01:58:22 +0200

    Modify Capacitor config for app name and plugins
    Updated app name and added server and plugins configuration.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,8 +2,18 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.solotask.app',
-  appName: 'solo-task',
-  webDir: 'dist'
+  appName: 'Solo Task',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample", // اختياري
+      iconColor: "#FF0000",
+      sound: "beep.wav",
+    },
+  },
 };
 
 export default config;
